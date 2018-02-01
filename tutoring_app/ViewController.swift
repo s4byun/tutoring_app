6cc57fcf3afe679aebab11926efb2f88d3cd348c
Daniel Byun <db186061@teradata.com>
2018-01-31 19:59:30 -0800

    Initial Commit

---
+++ tutoring_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tutoring_app
+//
+//  Created by Byun, Daniel on 1/31/18.
+//  Copyright © 2018 danielbyun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
